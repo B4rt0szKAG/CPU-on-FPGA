5ed47b15d6b73e9faed7544afd6b25b4c0320f88
B4rt0szKAG <bkaganiec@student.agh.edu.pl>
2026-04-16 17:16:41 +0200

    test2

---
+++ output_files/thesis.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 25.1std.0 Build 1129 10/21/2025 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10M50DAF484) Path("/home/jdo3/Desktop/studia/inżynierka/CPU-on-FPGA/output_files/") File("thesis.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- thesis.v
+++ thesis.v
@@ -1,9 +1,28 @@
 module thesis (
-	input sw0,
+	input  clk,
+	input  sw0,
+	input  rxd,
 	output led0,
 	output led1
 );
+	
+	uart_rxd reciver (
+		.clk(clk),
+		.rx(rxd),
+		.out(led1)
+	);
+	
 	assign led0 = sw0;
-	assign led1 = !sw0;
 
+endmodule
+
+module uart_rxd (
+	input wire clk,
+	input wire rx,
+	output reg out
+);
+	always @(posedge clk) begin
+		out <= rx;
+	end
+	
 endmodule
